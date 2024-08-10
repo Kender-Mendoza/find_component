d90d995ee99fcb563b7fc6104215940aef754122
Kender <kendermendoza591@gmail.com>
2024-08-10 18:03:25 +0200

    Using ts-node

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log("Esto es una prueba desde ts");
-console.log(process.argv.slice(2));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 console.log("Esto es una prueba desde ts");
 console.log(process.argv.slice(2))
+console.log("Esto es otra prueba")
+console.log("Esto es otra de otra prueba")
