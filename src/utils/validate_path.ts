d8611489efd0c507cb8dddf5e795535e2f8905f6
Kender <kendermendoza591@gmail.com>
2024-08-11 17:46:36 +0200

    added waiting module and spinnies

--- src/utils/validate_path.ts
+++ src/utils/validate_path.ts
@@ -1,5 +1,6 @@
 import { existsSync, statSync } from 'fs';
 import { resolve } from 'path';
+import { addWaiting, changeWaitingStatus } from './waiting'
 
 const isValidPath = (dirPath: string): boolean => {
   return existsSync(dirPath) && statSync(dirPath).isDirectory();
@@ -7,10 +8,13 @@ const isValidPath = (dirPath: string): boolean => {
 
 const validatePath = (dirPath: string): void => {
   const resolvedPath = resolve(dirPath);
+  const spinnies = addWaiting('Validating...');
 
   if (!isValidPath(resolvedPath)) { return; };
 
-  throw new Error(`The path: ${dirPath} is invalid.`);
+  changeWaitingStatus(spinnies, 'test');
+  console.log(`The path: ${dirPath} is invalid.`)
+  // throw new Error(`The path: ${dirPath} is invalid.`);
 };
 
 export default validatePath
